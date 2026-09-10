6ab41effc7a31ba4b37d8b6c647289e807db4d25
AlvaroCoe <Alvarodlcoelho@gmail.com>
2026-09-10 20:20:38 -0300

    mudar senha

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import AlterarSenha from "./pages/AlterarSenha";
 import HomePage from "./pages/HomePage";
 import ListaChamados from "./pages/ListaChamados";
 import AdminFuncionarios from "./pages/AdminFuncionarios";
@@ -26,6 +27,7 @@ export default function App() {
               <Route path="/redefinir-senha" element={<ResetPassword />} />
               <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
               <Route path="/chamados" element={<PrivateRoute><ListaChamados /></PrivateRoute>} />
+              <Route path="/alterar-senha" element={<PrivateRoute><AlterarSenha /></PrivateRoute>} />
               <Route path="/admin/funcionarios" element={<AdminRoute><AdminFuncionarios /></AdminRoute>} />
             </Routes>
           </main>
