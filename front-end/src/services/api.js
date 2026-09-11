c34aa585553bce75703d3a4c381c7926aaf3a407
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-11 20:34:45 -0300

    railaw

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+// Durante o desenvolvimento:
+// http://localhost:8080
+//
+// Quando o projeto estiver no Railway:
+// usaremos a variável VITE_API_URL
+export const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
@@ -8,9 +14,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
