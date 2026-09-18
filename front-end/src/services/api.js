a5acac230c1ccb5994280337ea40dd087c91184d
xstephaniexsouzax <stephaniesis024@gmail.com>
2026-09-18 15:44:36 -0300

    Railway

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,17 +1,8 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default api;
+// ... resto do arquivo continua igual
